6d61b6a331d60d7f66f013c6ef50585b09dbcc76
andreas.heger@gmail.com <andreas.heger@gmail.com>
2009-06-01 20:04:26 +0000

    Windows installation
    --HG--
    rename : lib/CorrespondenceAnalysis.py => SphinxReport/CorrespondenceAnalysis.py
    rename : lib/DataTypes.py => SphinxReport/DataTypes.py
    rename : lib/Histogram.py => SphinxReport/Histogram.py
    rename : lib/Plotter.py => SphinxReport/Plotter.py
    rename : lib/Renderer.py => SphinxReport/Renderer.py
    rename : lib/SQLDatabase.py => SphinxReport/SQLDatabase.py
    rename : lib/Stats.py => SphinxReport/Stats.py
    rename : lib/Tracker.py => SphinxReport/Tracker.py
    rename : lib/__init__.py => SphinxReport/__init__.py
    rename : lib/inheritance_diagram.py => SphinxReport/inheritance_diagram.py
    rename : lib/only_directives.py => SphinxReport/only_directives.py
    rename : lib/report_directive.py => SphinxReport/report_directive.py
    rename : lib/templates/Discussion.rst => SphinxReport/templates/Discussion.rst
    rename : lib/templates/Introduction.rst => SphinxReport/templates/Introduction.rst
    rename : lib/templates/Makefile => SphinxReport/templates/Makefile
    rename : lib/templates/Methods.rst => SphinxReport/templates/Methods.rst
    rename : lib/templates/PipelineTest.rst => SphinxReport/templates/PipelineTest.rst
    rename : lib/templates/Results.rst => SphinxReport/templates/Results.rst
    rename : lib/templates/Trackers.py => SphinxReport/templates/Trackers.py
    rename : lib/templates/Trackers.rst => SphinxReport/templates/Trackers.rst
    rename : lib/templates/analysis.rst => SphinxReport/templates/analysis.rst
    rename : lib/templates/conf.py => SphinxReport/templates/conf.py
    rename : lib/templates/contents.rst => SphinxReport/templates/contents.rst
    rename : lib/templates/gallery.html => SphinxReport/templates/gallery.html
    rename : lib/templates/index.html => SphinxReport/templates/index.html
    rename : lib/templates/indexsidebar.html => SphinxReport/templates/indexsidebar.html
    rename : lib/templates/labbook.css => SphinxReport/templates/labbook.css
    rename : lib/templates/layout.html => SphinxReport/templates/layout.html
    rename : lib/templates/pipeline.rst => SphinxReport/templates/pipeline.rst
    rename : lib/templates/search.html => SphinxReport/templates/search.html
    rename : lib/templates/theme.conf => SphinxReport/templates/theme.conf
    rename : lib/templates/usage.rst => SphinxReport/templates/usage.rst
    extra : convert_revision : svn%3A1e2bf9a2-43dd-11de-baa7-5503b21fc9cb/trunk%4024

---
+++ SphinxReport/clean.py
@@ -0,0 +1,82 @@
+#!/bin/env python
+import sys, os, imp, cStringIO, re, types, glob, optparse, shutil
+
+USAGE = """python %s [OPTIONS] target
+
+clean all data according to target.
+
+""" % sys.argv[0]
+
+from SphinxReport.Tracker import Tracker
+from SphinxReport.Renderer import *
+
+if not os.path.exists("conf.py"):
+    raise IOError( "could not find conf.py" )
+
+execfile( "conf.py" )
+
+SEPARATOR="@"
+
+def removeTracker( tracker ):
+    
+    # get locations
+    dirs_to_check = ("_static", "_cache", "_build" )
+
+    # image and text files
+    rx1 = re.compile("-%s%s" % (tracker,SEPARATOR) )
+    # files in cache
+    rx2 = re.compile("^%s$" % (tracker) )
+    # .code files
+    rx3 = re.compile("-%s%s" % (tracker,".code") )
+
+    isTracker = lambda x: rx1.search(x) or rx2.search(x) or rx3.search(x)
+
+    nremoved = 0
+    for d in dirs_to_check:
+        for root, dirs, files in os.walk(d):
+            for f in files:
+                if isTracker( f ):
+                    try:
+                        os.remove( os.path.join( root, f) )
+                        nremoved += 1
+                    except OSError, msg:
+                        pass
+
+    return nremoved
+
+def main():
+
+    parser = optparse.OptionParser( version = "%prog version: $Id$", usage = USAGE )
+
+    parser.set_defaults()
+        
+    (options, args) = parser.parse_args()
+
+    if len(args) == 0: 
+        print USAGE
+        raise UsageError("please supply at least one target.""")
+
+    if len(args) == 1 and args[0] in ("clean", "distclean", "cache"):
+        dirs = []
+        target = args[0]
+        if target in ("clean", "distclean"):
+            dirs.append( "_build" )
+        
+        if target in ("cache", "distclean"):
+            dirs.append( "_cache" )
+
+        if target in ("distclean",):
+            dirs.append( "_static/report_directive" )
+
+        for d in dirs:
+            if os.path.exists(d):
+                shutil.rmtree( d )
+
+    else:
+        for tracker in args:
+            print "cleaning up %s ..." % tracker,
+            nremoved = removeTracker( tracker )
+            print "%i files (done)" % nremoved
+                  
+if __name__ == "__main__":
+    sys.exit(main())
