58726bfeedaf86534362d822594352c6ed88c7f2
andreas.heger <none@none>
2009-07-07 12:21:11 +0000

    added more tests
    --HG--
    extra : convert_revision : svn%3A1e2bf9a2-43dd-11de-baa7-5503b21fc9cb/trunk%4056

--- doc/python/TestCases.py
+++ doc/python/TestCases.py
@@ -21,3 +21,34 @@ class LongLabelsSmall( Tracker ):
             data.append( (label, random.randint( 0,100 )) )
         return data
 
+class LargeMatrix( Tracker ):
+    """example of a large matrix with long labels."""
+    mWordSize = 5
+    mNumWords = 5
+    mNumTracks = 50
+
+    def getSlices( self, subset = None ): return "small", "large"
+    def getTracks( self ): return ["track%i" % i for i in range(self.mNumTracks)]
+    @returnLabeledData
+    def __call__(self, track, slice = None):
+        if slice == "small": ncolumns = 10
+        elif slice == "large": ncolumns = 40
+
+        data = []
+        for x in range( 0, ncolumns):
+            label = "%s:%i %s" % ( slice, x, " ".join([ "a" * self.mWordSize for y in range(self.mNumWords) ] ))
+            data.append( (label, random.randint( 0,100 )) )
+        return data
+
+class LayoutTest( Tracker ):
+    """Layout testing."""
+    mNumSlices = 3
+    mNumTracks = 5
+    mNumSamples = 100
+
+    def getSlices( self, subset = None ): return ["slice%i" % i for i in range(self.mNumSlices)]
+    def getTracks( self ): return ["track%i" % i for i in range(self.mNumTracks)]
+
+    @returnSingleColumnData
+    def __call__(self, track, slice = None):
+        return [ random.gauss( 0,1 ) for x in range(self.mNumSamples) ]
