dcb3c52d59d1572bfec1cdb2444c5947ff431550
Andreas Heger <andreas.heger@gmail.com>
2014-02-05 15:00:27 +0000

    travis: testing different requirements

--- setup-DEBs.sh
+++ setup-DEBs.sh
@@ -2,4 +2,4 @@
 
 # DEB prerequisites
 apt-get install -y gcc g++ zlib1g-dev libssl-dev libbz2-dev libfreetype6-dev libpng12-dev libblas-dev libatlas-dev liblapack-dev gfortran libpq-dev r-base-dev libreadline-dev libmysqlclient-dev libboost-dev libsqlite3-dev mercurial
-apt-get install -y python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-nose
+apt-get install -y python-numpy python-scipy python-matplotlib
