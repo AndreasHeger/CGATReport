bb52cb07a6d7a56dd637e326256a2247c766f321
Andreas Heger <andreas.heger@gmail.com>
2014-10-16 11:31:51 +0100

    pep8

--- tests/SphinxReport_test.py
+++ tests/SphinxReport_test.py
@@ -3,12 +3,7 @@
 '''
 
 import unittest
-import os, re, sys
-import itertools
-import collections
-import subprocess
-import shutil
-import logging
+import sys
 
 IS_PYTHON3 = sys.version_info[0] >= 3
 
@@ -16,22 +11,23 @@ import SphinxReport
 import SphinxReport.Utils
 import SphinxReport.Dispatcher
 
+
 class SphinxReportTest(unittest.TestCase):
     ''' '''
     renderer = "table"
     tracker = "tests.TestTrackers.LabeledDataExample"
-    
-    def testTracker( self ):
-        
-        code, tracker = SphinxReport.Utils.makeTracker( self.tracker, (), {} )
-        renderer = SphinxReport.Utils.getRenderer( self.renderer, {} )
 
-        dispatcher = SphinxReport.Dispatcher.Dispatcher( tracker,
-                                                         renderer,
-                                                         [] )
-        results = dispatcher( {} )
+    def testTracker(self):
+        code, tracker, tracker_path = SphinxReport.Utils.makeTracker(
+            self.tracker, (), {})
+        renderer = SphinxReport.Utils.getRenderer(
+            self.renderer, {})
+
+        dispatcher = SphinxReport.Dispatcher.Dispatcher(
+            tracker,
+            renderer,
+            [])
+        results = dispatcher({})
         print (results)
         return True
 
-    
-    
