930b6d77ebb30a27820ea3d3b98fdfefcb5a4c9e
Andreas Heger <andreas.heger@gmail.com>
2015-07-23 12:44:37 +0100

    change pandas2ri to py2ri for rpy 2.6

--- CGATReportPlugins/RPlotter.py
+++ CGATReportPlugins/RPlotter.py
@@ -17,7 +17,7 @@ from CGATReport import Stats, Utils
 try:
     from rpy2.robjects import r as R
     import rpy2.robjects as ro
-    import rpy2.robjects.py2ri
+    import rpy2.robjects.pandas2ri
     rpy2.robjects.pandas2ri.activate()
     import rpy2.robjects.numpy2ri
     rpy2.robjects.numpy2ri.activate()
